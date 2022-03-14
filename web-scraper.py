f1781c6af226115fa2bf07b01cbb03b6fd06538d
Justin Baksi <justinbaksi@justins-air-3.mynetworksettings.com>
2022-03-13 23:48:13 -0400

    update web-scraper.py

--- web-scraper.py
+++ web-scraper.py
@@ -1,47 +1,67 @@
 #!/opt/homebrew/bin/python3
 
-# Author(s): Shaun Derstine
+# Author(s): Shaun Derstine and Justin Baksi
 # Last Edit: 
-# Description: 
+# Description:
 
-from bs4 import BeautifulSoup as bs
-from urllib.request import urlopen
-from sys import argv
-from datetime import datetime
+from urllib.request import urlopen as request
+from bs4 import BeautifulSoup as soup
+from datetime import date, datetime
+import mysql.connector
 
-def convert_url(url):
-	# urlopen makes a request to webpage at url
-	# result is an object which is saved as url_object
-	url_object = urlopen(url)
 
-	# the raw html from the webpage is saved in html_doc
-	html_doc = url_object.read()
+#connect to the database
+mydb = mysql.connector.connect(
+  host="localhost",
+  user="root",
+  password="JuanSoto^%OPS+",
+  database= "gpu"
+)
+mycursor = mydb.cursor()
 
-	# converts raw html to BeautifulSoup object 'soup'
-	soup = bs(html_doc, 'html.parser')
+page = 1
+while True:
+	now = datetime.now()
+	formatted_date = now.strftime('%Y-%m-%d %H:%M:%S')
 
-	return soup
-# end convert_url()
+	#there are only 100 pages for GPUs currently on newegg, so we will reset the count to 1 when we exceed the limit
+	if page == 101:
+		page = 1
 
-def main():
-	# url is saved as a string
-	url = argv[1]
+	#the URL which we want to scrape. this is the page to preview all the graphics card. the page number will increment by one to account for all the pages.
+	url = 'https://www.newegg.com/p/pl?d=graphics+cards&page=' + str(page)
 
-	soup = convert_url(url)
+	##grab the html from the page
+	client = request(url)
+	page_html = client.read()
+	client.close()
+	page_soup = soup(page_html, 'html.parser')
+	cells = page_soup.find_all("div", attrs={"class": "item-cell"})
 
-	# li element that holds current price info
-	li_elem = soup.find("li", class_="price-current")
 
-	cur_price = ""
+	#here we loop through each cell with "class = "item-cell"". on newegg, these are each gpu listed.
+	for cell in cells:
 
-	# starting from index 1 in the children of the li element,
-	# $, dollar amount, cent amount
-	for i in range(1, len(li_elem.contents)):
-		cur_price += li_elem.contents[i].string
+		try:
+			full_name = str(cell.find_all("a", attrs={"title": "View Details"})).split('>')[1].split('<')[0]
+			#brand_name = full_name.split()[0]
+			#print(page, i, brand_name, date.today().isoformat(), datetime.now().time())
+		except:
+			print("not a gpu")
 
-	print("Price as of %s: %s" % (datetime.now(), cur_price))
-	# store in dictionary { "Date":datetime.now(), "Price":cur_price }
-# end main() 
+		#insert the entry into our database
+		try:
+			sql = "INSERT INTO prices (gpuname, price, datetime) VALUES (%s, %s, %s)"
+			val = (full_name, 600, formatted_date)
+			mycursor.execute(sql, val)
+			print("inserted entry into database")
+		except: #if that gpu is already in our DB, we update the price and date :)
+			sql = "UPDATE prices SET price = %s, datetime = %s WHERE gpuname = %s"
+			val = (420.69, formatted_date, full_name)
+			mycursor.execute(sql, val)
+			print("updated")
+	mydb.commit()
 
-if __name__ == "__main__":
-	main()
+	#increment page by one
+	page += 1
+
