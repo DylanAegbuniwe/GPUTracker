0d31aee2b0c3a2cf6216a3ebe797e4aad4ad65ba
0xdecafdad <shaunderstine@gmail.com>
2022-04-20 11:26:47 -0400

    demo_scraper.py prints out key, value pairs of dictionary returned by get_best_selling_gpus() line by line

--- demo_scraper.py
+++ demo_scraper.py
@@ -1,7 +1,12 @@
 import scraper
 
 def main():
-	scraper.get_best_selling_gpus()
+	# Get product details for top selling GPUs
+	product_details = scraper.get_best_selling_gpus()
+	
+	# Print key, value pairs line by line
+	for key, value in product_details.items():
+		print(key, ":", value)
 
 if __name__ == "__main__":
 	main()
