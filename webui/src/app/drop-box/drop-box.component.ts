75d74d67fe75050081e50f8ea779493878376cb3
Justin Baksi <justinbaksi@justins-air-3.myfiosgateway.com>
2022-04-25 12:09:44 -0400

    update webui

--- webui/src/app/drop-box/drop-box.component.ts
+++ webui/src/app/drop-box/drop-box.component.ts
@@ -1,43 +1,39 @@
+import { HttpClient } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
+
+export class GPU {
+  constructor(
+    public datetime: string,
+    public gpuname: string,
+    public page: number,
+    public price: number
+  ) {}
+   getName(){
+    return this.gpuname;
+  }
+}
+
 
 @Component({
   selector: 'app-drop-box',
   templateUrl: './drop-box.component.html',
   styleUrls: ['./drop-box.component.css']
+  
 })
 export class DropBoxComponent implements OnInit {
-  names: string[] = ["Affogato",
-  "Americano",
-  "Bicerin",
-  "Breve",
-  "Café Bombón",
-  "Café au lait",
-  "Caffé Corretto",
-  "Café Crema",
-  "Caffé Latte",
-  "Caffé macchiato",
-  "Café mélange",
-  "Coffee milk",
-  "Cafe mocha",
-  "Cappuccino",
-  "Carajillo",
-  "Cortado",
-  "Cuban espresso",
-  "Espresso",
-  "Eiskaffee",
-  "The Flat White",
-  "Frappuccino",
-  "Galao",
-  "Greek frappé coffee",
-  "Iced Coffee﻿",
-  "Indian filter coffee",
-  "Instant coffee",
-  "Irish coffee",
-  "Liqueur coffee"
-];
-  constructor() { }
+  gpus: GPU[] = [];
+
+  constructor(private httpClient: HttpClient) { }
 
   ngOnInit(): void {
+    this.getGPUs();
   }
-
+  getGPUs(){
+    this.httpClient.get<any>('http://127.0.0.1:5000/').subscribe(
+      response => {
+        console.log(response);
+        this.gpus = response
+      }
+    );
+  }
 }
